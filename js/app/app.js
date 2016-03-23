c99d9cfd0c8b9663c723e4aae10fa9345e7cb2cf
Todd Motto <todd@toddmotto.com>
2016-03-23 17:45:22 +0000

    chore: Added solution

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,23 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+	.config(function ($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: 'views/home.html'
+			})
+			.state('home.notifications', {
+				url: 'notifications',
+				templateUrl: 'views/home/notifications.html'
+			})
+			.state('home.timeline', {
+				url: 'timeline',
+				templateUrl: 'views/home/timeline.html'
+			})
+			.state('home.user', {
+				url: 'user',
+				templateUrl: 'views/home/user.html'
+			});
+
+		$urlRouterProvider.otherwise('/');
+	});
