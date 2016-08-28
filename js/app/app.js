d27299871449e6ea3ed08d535d267af4bb7520b7
Ziv Zamechek <ziv921@yahoo.com>
2016-08-28 10:08:10 -0400

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,25 @@
 angular
-    .module('app', ['']);
+    .module('app', [''])
+    .config(function ($stateProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'views/home.html',
+          controller: 'SettingController'
+        })
+        .state('home.notifications', {
+          url: 'notifications',
+          templateUrl: 'views/home/notifications.html',
+          controller: 'UserSettingsController'
+        })
+        .state('home.timeline', {
+          url: 'timeline',
+          templateUrl: 'views/home/timeline.html',
+          controller: 'NotificationsSettingsController'
+        })
+        .state('home.user' {
+          url: 'user',
+          templateUrl: 'views/home/user.html',
+          controller: 'NotificationsSettingsController'
+        })
+    });
